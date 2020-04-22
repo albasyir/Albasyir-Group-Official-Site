cbf94d2e77953cec466e857083e818526991e49b
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2020-04-22 18:45:12 +0700

    WIP : ubah slide

--- components/Slider/SimpleText.vue
+++ components/Slider/SimpleText.vue
@@ -5,7 +5,7 @@
     margin: 0;
   }
 
-  .slide-desc-text {
+  .slide-desc {
     position: absolute;
     right: 0;
     bottom: 0;
@@ -22,7 +22,7 @@
   }
 
   @media screen and (max-width: 950px) {
-     .slide-desc-text {
+     .slide-desc {
       position: relative;
       width: 100%;
       font-size: 20pt;
@@ -41,24 +41,16 @@
     <v-container>
       <v-row no-gutters id='slide-container'>
         <div
-          v-for='(slide_row, key) in slidesData'
-          :key="key"
-          :id='`slide-${key}`'
           class='d-none fade py-3 col-12'
-          style='opacity: 0'
-          :ref="`slide-${key}`"
         >
-          <div class='slide-title-text white--text'>
-            <div
-              v-for="(part_title, key) in slide_row.title.split(' ')"
-              :key="key"
-            >
-              {{ part_title }}
-            </div>
+          <div class='slide-title-text white--text' ref="slideTitle">
+            <!-- TEXT TITILE HERE -->
           </div>
-          <div class='slide-desc-text white--text'>
+          <div class='slide-desc white--text'>
             <div class='slide-desc-line white' />
-            {{ slide_row.desc }}
+            <div class='slide-desc-text' ref="slideDesc">
+              <!-- TEXT DESC HERE -->
+            </div>
           </div>
         </div>
       </v-row>
@@ -68,119 +60,132 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, Ref } from 'nuxt-property-decorator'
+import Typer from "typer-js"
+
+/**
+ * interface for each TextSlide Data
+ */
+export interface TextSlideInterface {
+  /**
+   * Slide description
+   */
+  desc: String,
+
+  /**
+   * Slide title
+   */
+  title: String
+}
+
+/**
+ * interface for array of TextSlide Data
+ */
+export interface ArrayTextSlideInterface {
+  /**
+   * Array of TextSlide TextSlideInterface Data
+   */
+  [index : number] : TextSlideInterface
+}
 
 @Component
-export default class SimpleText extends Vue {
-  @Prop({ type: Array, required: true }) slidesData !: Array<Object>
- 
-  slideObject : any =  null
-  slideActive : number = 0
-  slideInterval : any = 5000
+export default class TextSlide extends Vue {
+  // slideData texting, remember with interface
+  @Prop({ type: Array, required: true }) slidesData !: ArrayTextSlideInterface
 
   /**
-   * 
-   * 
+   * Slide Configuration
    */
-  slideStart() {
-    this.slideActive = 0
+  slideActive : number = 0 // start from ?
+  slideInterval : number = 5000 // what is interval each slide ?
+
+
+  /**
+   * object can be use for intervalTimeObject
+   */
+  slideObjectInterval : ReturnType<typeof setTimeout> | undefined;
+
+
+  /**
+   * Start this slide
+   *
+   * this method will do : \n
+   *   1. stop slide (if already run)
+   *   2. mount slide
+   *   3. set interval from "slideInterval"
+   *   4. next slide
+   *
+   */
+  slideStart() : void {
+    // stop
+    this.slideStop()
 
     this.mountSlide()
 
-    let intervalSlide = setInterval(() => {
+    this.slideObjectInterval = setInterval(() => {
+
       this.nextSlide()
     }, this.slideInterval)
-
-    return intervalSlide
   }
 
-
   /**
-   * 
-   * 
-   */
-  mountSlide() {
-    let slide : HTMLElement = this.activeSlide();
-
-    if(slide) {
-      setTimeout(() => {
-        slide.classList.remove("d-none")
-        slide.style.opacity = "1"
-      }, 2000)
-    }
-
-    
-  }
-
-
-  /**
-   * 
-   * 
-   */
-  activeSlide() {
-    let el = document.getElementById(`slide-${this.slideActive}`)
-
-    if(!el) {
-      this.slideActive = 0
-      el = document.getElementById(`slide-${this.slideActive}`)
-    }
-
-    return el as HTMLElement
-  }
-
-
-  /**
-   * 
-   * 
-   */
-  async leaveSlide() {
-    let slide : HTMLElement = this.activeSlide()
-
-    if(slide) {
-      slide.style.opacity = "0"
-
-      await setTimeout(() => {
-        slide.classList.add("d-none")
-      }, 2000)
-    }
-  }
-
-
-  /**
-   * 
-   * 
+   * Next slide
+   *
+   * this method will do : \n
+   *   1. leaveSlide()
+   *   2. increment slide ()
+   *   3. mountSlide()
    */
   nextSlide() {
+    // leave active slide
     this.leaveSlide()
+
+    // next index of slide
     this.slideActive = this.slideActive + 1
+
+    // mount new slide
     this.mountSlide()
   }
 
+
   /**
-   * 
-   * 
+   *
+   *
+   */
+  mountSlide() {
+    let data : TextSlideInterface = this.slidesData[this.slideActive];
+  }
+
+
+  /**
+   *
+   *
+   */
+  async leaveSlide() {
+
+  }
+
+
+  /**
+   *
+   *
    */
   slideStop() {
-    clearInterval(this.slideObject);
+    clearInterval(this.slideObjectInterval!) // use ! because interval can be nullable
   }
 
 
   /**
-   * 
-   * 
+   *
+   *
    */
   mounted() {
-    if(!this.$props.slidesData) {
-      console.error('Slide Text cant be run.., because slide data not available')
-      return;
-    }
-
-    this.slideObject = this.slideStart()
+    this.slideStart();
   }
 
 
   /**
-   * 
-   * 
+   *
+   *
    */
   beforeDestroy() {
     this.slideStop();
--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,4 +1,9 @@
 declare module "*.vue" {
   import Vue from 'vue'
+
   export default Vue
+}
+
+declare module 'typer-js' {
+
 }
